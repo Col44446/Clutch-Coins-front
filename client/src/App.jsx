8db339002e8654d420a036387f7e63fb311439ce
Harman Sigh <harmansinghcholtra@gmail.com>
2025-08-23 04:11:30 -0700

    fix: header import issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import "./App.css";
 import Header from "./components/common/Header";
 import { Home } from "./pages/Home";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
